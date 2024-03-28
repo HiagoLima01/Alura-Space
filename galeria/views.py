e5f0d77d810610a17d422e238302259a45795df4
HiagoLima01 <hiago1234657@gmail.com>
2024-03-28 14:40:20 -0300

    Adicionando pasta templates para html

--- galeria/views.py
+++ galeria/views.py
@@ -2,7 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse(
-        '<h1>Alura Space</h1>'
-    )
-
+    return render(request, 'index.html')
