b8172863366bd32f5990259fb628649ae2f1db87
HiagoLima01 <hiago1234657@gmail.com>
2024-03-29 19:40:01 -0300

    Criando estrutura de retição no index.html para imagens

--- galeria/views.py
+++ galeria/views.py
@@ -2,8 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+
+
 def index(request):
-    return render(request, 'galeria/index.html')
+
+    dados = {
+    1: {"nome": "Nebulosa de Carina",
+            "legenda": "webbtelescope.org / NASA / James Webb"},
+    2: {"nome": "Galáxia NGC 1079",
+            "legenda": "nasa.org / NASA / Hubble"}
+    }
+
+    return render(request, 'galeria/index.html', {"cards":dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
