04b23f67b5afbd0dd3a7b7364180a5e656a3d97e
HiagoLima01 <hiago1234657@gmail.com>
2024-03-30 11:15:50 -0300

    Iniciando classe para obter dados 'models'

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Fotografia(models.model):
+    nome = models.CharField(max_Length=100, null=False, blank=False)
+    legends = models.CharField(max_Length=100, null=False, blank=False)
+    descricao = models.TextField(null=False, blank=False)
+    foto = models.CharField(max_Length=100, null=False, blank=False)
