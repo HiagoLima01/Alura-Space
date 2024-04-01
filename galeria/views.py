8c01b9808faca18b7af271d4dfefdcb53146c853
Hiago Lima <hiago1234657@gmail.com>
2024-04-01 20:29:35 -0300

    Melhorando a página Admin

--- galeria/views.py
+++ galeria/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render, get_object_or_404
 from galeria.models import Fotografia
 
 def index(request):
-    fotografias = Fotografia.objects.all()
+
+    fotografias = Fotografia.objects.order_by('data_fotografia').filter(publicado=True)
+
     return render(request, 'galeria/index.html', {"cards": fotografias})
 
 def imagem(request, foto_id):
