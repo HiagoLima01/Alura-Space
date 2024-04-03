c7818948f9a9e0686c5bcb61be717056e13fad67
HiagoLima01 <hiago1234657@gmail.com>
2024-04-03 19:55:01 -0300

    Adicionando páginas de usuários no template e arrumando URLs

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from usuarios.views import login, cadastro
 
 
 urlpatterns = [
+    path('usuario/login', login, name='login'),
+    path('usuario/cadastro', cadastro, name='cadastro'),
+    
+
 
 ]
